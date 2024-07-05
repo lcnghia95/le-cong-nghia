36fcd5ab2590163f531025eb7f5a227adcf8aabb
lcnghia95 <lenghia@escorti.com.vn>
2024-07-06 02:18:50 +0700

    fix: remove comment

--- src/problem5/src/presentation/http/middlewares/validator.ts
+++ src/problem5/src/presentation/http/middlewares/validator.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { validationResult } from 'express-validator';
 
-// Middleware để validate body của request
 export const validateBody = (validations: any[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     await Promise.all(validations.map((validation) => validation.run(req)));
